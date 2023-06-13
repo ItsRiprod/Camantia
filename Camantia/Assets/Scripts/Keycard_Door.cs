301d530d17daea51b70b18a6862ab130eef6cfe3
Doravid <doravidmc@gmail.com>
2023-06-13 12:33:46 -0400

    DOOR :100: :alien: :aries:

--- Camantia/Assets/Scripts/Keycard_Door.cs
+++ Camantia/Assets/Scripts/Keycard_Door.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class Keycard_Door : MonoBehaviour
 {
-    public GameObject door;
-    public Transform start, end;
+
+    [SerializeField] private Animator animDoor = null;
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+    private bool isOpen = false;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        transform
+    
+ 
+        //transform.rotation = start.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E) && !isOpen)
         
+        {
+            animDoor.Play("Door_Open_Keycard", 0, 0.0f);
+            isOpen = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.E) && isOpen)
+        {
+            animDoor.Play("Door_Close_Keycard", 0, 0.0f);
+            isOpen = false;
+        }
+
+
     }
 }
