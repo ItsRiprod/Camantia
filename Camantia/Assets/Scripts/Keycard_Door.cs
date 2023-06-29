db0e1a167d52f5571c924c40eafde556e12f03ac
Doravid <74063222+Doravid@users.noreply.github.com>
2023-06-29 14:18:53 -0400

    Door now doesn't have collision on opening
    title

--- Camantia/Assets/Scripts/Keycard_Door.cs
+++ Camantia/Assets/Scripts/Keycard_Door.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Keycard_Door : MonoBehaviour
@@ -14,6 +15,8 @@ public class Keycard_Door : MonoBehaviour
     public Transform lookingAt;
     public float doorOpenRange;
     private float timeTillClose = 0;
+    private double noCollisionTime = 0;
+    public GameObject doorCollision;
 
     
 
@@ -28,7 +31,14 @@ public class Keycard_Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (!doorCollision.GetComponent<Collider>().enabled)
+        {
+            noCollisionTime -= Time.deltaTime;
+        }
+        if (noCollisionTime <= 0)
+        {
+            doorCollision.GetComponent<Collider>().enabled = true;
+        }
 
         //Opens the door if you have a key card
         if (Input.GetKeyDown(KeyCode.E) && !isOpen && inventory.GetAmountOfItem(keycard) >= 1)
@@ -39,15 +49,23 @@ public class Keycard_Door : MonoBehaviour
                 animDoor.Play("Door_Open_Keycard", 0, 0.0f);
                 isOpen = true;
                 timeTillClose += 5;
+                TurnOffCollision();
             }
+
 
             
         }
+
         else if(Input.GetKeyDown(KeyCode.E) && isOpen && inventory.GetAmountOfItem(keycard) >= 1)
         {
-            animDoor.Play("Door_Close_Keycard", 0, 0.0f);
-            isOpen = false;
-            timeTillClose = 0;
+            RaycastHit hit;
+            if (Physics.Raycast(lookingAt.transform.position, lookingAt.transform.TransformDirection(Vector3.forward), out hit, doorOpenRange))
+            {
+                animDoor.Play("Door_Close_Keycard", 0, 0.0f);
+                isOpen = false;
+                timeTillClose = 0;
+                TurnOffCollision();
+            }
         }
 
         if (isOpen)
@@ -58,9 +76,17 @@ public class Keycard_Door : MonoBehaviour
             {
                 animDoor.Play("Door_Close_Keycard", 0, 0.0f);
                 isOpen = false;
+                TurnOffCollision();
             }
         }
+ 
 
 
+    }
+
+    private void TurnOffCollision()
+    {
+        noCollisionTime = .5;
+        doorCollision.GetComponent<Collider>().enabled = false;
     }
 }
